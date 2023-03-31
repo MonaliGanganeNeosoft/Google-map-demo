7ba8969105c787dba15ba69fcba20252109cbb46
Monali Gangane <90671888+MonaliGanganeNeosoft@users.noreply.github.com>
2023-03-31 16:46:19 +0530

    for google map demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,163 @@
-import logo from './logo.svg';
-import './App.css';
+// import * as React from "react";
+// import { useMemo } from "react";
+// import "./App.css";
+// import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+// // import Map from "./components/map";
+// export default function App() {
+//   const { isLoaded } = useLoadScript({
+//     googleMapsApiKey: "AIzaSyCqrPHicvr3jEIWWcfs05-QOb-aOxWTxs4",
+//     // libraries: ["places"],
+//   });
+//   if (!isLoaded) return <div>Loading...</div>;
+//   return <Map />;
+// }
 
-function App() {
+// function Map() {
+//   const center = useMemo(() => ({ lat: 18.5204, lng: 73.8567 }), []);
+//   return (
+//     <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+//       <Marker position={center} />
+//     </GoogleMap>
+//   );
+// }
+
+import * as React from "react";
+import "./App.css";
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  HStack,
+  IconButton,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import { FaLocationArrow, FaTimes } from "react-icons/fa";
+import { useState, useRef } from "react";
+
+const center = { lat: 18.5204, lng: 73.8567 };
+export default function App() {
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const originRef = useRef();
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const destiantionRef = useRef();
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyCqrPHicvr3jEIWWcfs05-QOb-aOxWTxs4",
+    libraries: ["places"],
+  });
+  if (!isLoaded) {
+    return <p>hii</p>;
+  }
+  async function calculateRoute() {
+    if (originRef.current.value === "" || destiantionRef.current.value === "") {
+      return;
+    }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService();
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destiantionRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+    setDirectionsResponse(results);
+    setDistance(results.routes[0].legs[0].distance.text);
+    setDuration(results.routes[0].legs[0].duration.text);
+  }
+  function clearRoute() {
+    setDirectionsResponse(null);
+    setDistance("");
+    setDuration("");
+    originRef.current.value = "";
+    destiantionRef.current.value = "";
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Flex
+        position="relative"
+        flexDirection="column"
+        alignItems="center"
+        h="100vh"
+        w="100vw"
+      >
+        <Box position="absolute" left={0} top={0} h="100%" w="100%">
+          {/* google map */}
+          <GoogleMap
+            center={center}
+            zoom={15}
+            mapContainerStyle={{ width: "100%", height: "100%" }}
+            options={{
+              zoomControl: false,
+              streetViewControl: false,
+              mapTypeControl: false,
+              fullscreenControl: false,
+            }}
+            onLoad={(map) => setMap(map)}
+          >
+            <Marker position={center} />
+            {directionsResponse && (
+              <DirectionsRenderer directions={directionsResponse} />
+            )}
+          </GoogleMap>
+        </Box>
+
+        <Box
+          p={4}
+          borderRadius="lg"
+          mt={4}
+          bgColor="white"
+          shadow="base"
+          minW="container.md"
+          zIndex="modal"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <HStack spacing={4}>
+            <Autocomplete>
+              <Input type="text" placeholder="Origin" ref={originRef} />
+            </Autocomplete>
+            <Autocomplete>
+              <Input
+                type="text"
+                placeholder="Destination"
+                ref={destiantionRef}
+              />
+            </Autocomplete>
+
+            <ButtonGroup>
+              <Button colorScheme="pink" type="submit" onClick={calculateRoute}>
+                Calculate Route
+              </Button>
+              <IconButton
+                aria-label="center back"
+                icon={<FaTimes />}
+                onClick={clearRoute}
+              />
+            </ButtonGroup>
+          </HStack>
+          <HStack spacing={4} mt={4} justifyContent="space-between">
+            <Text>Distance:{distance} </Text>
+            <Text>Duration:{duration} </Text>
+            <IconButton
+              aria-label="center back"
+              icon={<FaLocationArrow />}
+              isRound
+              onClick={() => map.panTo(center)}
+            />
+          </HStack>
+        </Box>
+      </Flex>
+    </>
   );
 }
-
-export default App;
